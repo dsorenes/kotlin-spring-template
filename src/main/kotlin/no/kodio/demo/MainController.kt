a53542d0037dc60265a6f4037371974da9829f2a
Daniel Sørenes <daniel@sorenes.no>
2022-09-08 14:49:34 +0200

    Use spring boot gcp starter for connecting to db

--- src/main/kotlin/no/kodio/demo/MainController.kt
+++ src/main/kotlin/no/kodio/demo/MainController.kt
@@ -1,13 +1,17 @@
 package no.kodio.demo
 
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class MainController {
+class MainController(val jdbcTemplate: JdbcTemplate) {
 
     @GetMapping("/")
-    fun index(): String {
-        return "hello"
+    fun index(): List<String> {
+        return jdbcTemplate.queryForList("SELECT * FROM demo").stream()
+            .map {
+                it.values.toString()
+            }.toList();
     }
 }
---
+++ src/main/resources/db/migration/V1.2__initial_values.sql
@@ -0,0 +1,4 @@
+INSERT INTO demo VALUES
+                      (1, 'luisao@example.com'),
+                      (2, 'jonas@example.com'),
+                      (3, 'fejsa@example.com');
